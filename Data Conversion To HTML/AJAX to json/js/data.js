246fb329ec33b069d33de77883a2949d2ad443a4
kelogay <129968289+kelogay@users.noreply.github.com>
2023-04-05 22:44:31 +0530

    Add files via upload

---
+++ Data Conversion To HTML/AJAX to json/js/data.js
@@ -0,0 +1,30 @@
+var DATA = [
+    {
+
+        "NAME": "Mahipal",
+        "AGE": 20,
+        "City": "Valsad",
+        "Country": "India"
+    },
+
+    {
+        "NAME": "Parth",
+        "AGE": 40,
+        "City": "Mumbai",
+        "Country": "India"
+    },
+
+    {
+        "NAME": "Jatvik",
+        "AGE": 69,
+        "City": "Delhi",
+        "Country": "India"
+    },
+
+    {
+        "NAME": "Divya",
+        "AGE": 99,
+        "City": "Gully Boy",
+        "Country": "India"
+    },
+];
